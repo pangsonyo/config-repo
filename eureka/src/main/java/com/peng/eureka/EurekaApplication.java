9b90edf6ae21373dea110573780f9560025d8725
pangsonyo <1015614034@qq.com>
2018-07-23 22:35:23 +0800

    Add files via upload
    Eureka Server

---
+++ eureka/src/main/java/com/peng/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.peng.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * eureka服务端
+ * 2018-07-05
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
